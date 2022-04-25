c7630239d7bc04844d6584ea1c56f51ab459777a
Ian Sui <ian.appdeture@gmail.com>
2022-04-25 16:25:24 +0800

    Minor Updates

--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,8 @@ export default function Home() {
   const [product, setProduct] = useState("")
   const [products, setProducts] = useState([])
   const [value, setValue] = useState("")
-  const [randomValue, setRandomValue] = useState(0)
   const deferredValue = useDeferredValue(value, { timeoutMs: 5000 })
   const deferredList = useDeferredValue(list, { timeoutMs: 5000 })
-
-
-  function handleClick() {
-    startTransition(() => {
-      setRandomValue(Math.random() * 1000000);
-    })
-  }
 
   function pushProject(event) {
     startTransition(() => {
@@ -37,12 +29,13 @@ export default function Home() {
     <Box pt={16}>
       <Container maxW="container.xl">
         <Heading mb={4}>React 18 New Hooks</Heading>
-        <Heading mb={2}>useId</Heading>
+        <Heading size="lg" mb={4}>Used for Application Code:</Heading>
+        <Heading size="md" mb={2}>useId</Heading>
         <HStack spacing={4} mb={8}>
           <Text>Checkbox Id: {id}</Text>
           <Checkbox id={id} />
         </HStack>
-        <Heading mb={2}>useTransition</Heading>
+        <Heading size="md" mb={2}>useTransition</Heading>
         <Input onChange={(e) => pushProject(e)} mb={2} />
         <Text mb={2}>Products</Text>
         <VStack align="start" spacing={0} mb={2}>
@@ -52,16 +45,21 @@ export default function Home() {
             ))
           }
         </VStack>
-        <Heading mb={2}>useDeferredValue</Heading>
+        <Heading size="md" mb={2}>useDeferredValue</Heading>
         <Input mb={2} onChange={(e) => setValue(e.target.value)} />
         <Text>Value: {value}</Text>
         <Text mb={2}>Deferred Value: {deferredValue}</Text>
         <Text>Deferred List:</Text>
+        <VStack align="start" spacing={0} mb={8}>
         {
           deferredList.map((item, itemKey) => (
             <Text key={itemKey}>{item}</Text>
           ))
         }
+        </VStack>  
+        <Heading size="lg" mb={4}>Used for Libraries:</Heading>
+        <Heading size="md" mb={4}>useSyncExternalStore</Heading>
+        <Heading size="md" mb={4}>useInsertionEffect</Heading>
       </Container>
     </Box>
   )
